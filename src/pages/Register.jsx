a5c5bc480eba676ffe3833ec17a6fad1b615aa43
kharateameya <kharateameya@gmail.com>
2023-06-13 23:09:35 +0530

    Design Login and Register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Add from "../img/image-icon.png"
+
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chat App</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input style={{display:"none"}} type="file" id="file"/>
+          <label htmlFor="file">
+            <img src={Add} alt="image-icon"/>
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>Do you have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
